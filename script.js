6cf6265bf68742a3d3dd46460f2ad67703d9b987
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-21 17:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+		let num = parseInt(c);
+	let output = {};
+   let unit = {
+	q: 25,
+	d: 10,
+	n: 5,
+	p: 1
+    };
+	for(let key in unit){
+		let curr = unit[key];
+		output[key] = Math.floor(num / curr);
+		num = num % curr;
+	}
+	return output;
 };
 
 // Do not the change the code below
